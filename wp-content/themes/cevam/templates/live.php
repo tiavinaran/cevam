f097b64a6d280a23e678e9ad7638dc46f4fb7ccb
Tiavina Ranaivoson <a.tiavina.ran@gmail.com>
2024-01-27 11:13:06 +0400

    update live page interface

--- wp-content/themes/cevam/templates/live.php
+++ wp-content/themes/cevam/templates/live.php
@@ -10,7 +10,12 @@ get_header();
     <h1>Live</h1>
     <section id="youtube-live">
         <iframe src="https://www.youtube.com/embed/live_stream?channel=<?php echo YOUTUBE_CHANNEL_ID; ?>&autoplay=1" frameborder="0" allowfullscreen allow="autoplay; encrypted-media"></iframe>
-        <section id="no-live">Aucun Live pour le moment</section>
+        <section id="no-live">
+            <span>Aucun Live pour le moment</span>
+            <a id="youtube-channel-link" href="https://www.youtube.com/@cevamnetwork" target="_blank">
+                <img src="<?php echo get_template_directory_uri() . '/assets/img/youtube.svg'; ?>" alt="play" title="Cevam network" />
+            </a>
+        </section>
     </section>
 </main>
 
